5bc89bddbacef821fc492ffa83a0d8f7070af370
tylerschlepko <97323708+tylerschlepko@users.noreply.github.com>
2023-02-27 18:04:46 -0600

    Made an initial recipes

--- seed.sql
+++ seed.sql
@@ -1,2 +1,73 @@
-INSERT INTO recipes (img_path, title, instructions, description, ingredients)
-VALUES ()
+INSERT INTO users (username, name, email, password)
+VALUES ('testUser', 'name', 'name@email.com', 'password');
+INSERT INTO recipes (img_path, title, instructions, description, ingredients, user_id)
+VALUES ('banana_bread.jpg','Protein Blueberry Banana Bread','Mix all of the ingredients 
+Bake at 350 for 22-30 minutes','Each Slice (10): 68 Calories | 6g Protein | 10.5g Carbs| .5g Fat
+Whole Loaf: 687 Calories | 57g Protein | 4g Fat | 105g Carbs', '2 bananas
+60g Flour
+100mL Almond Milk
+2 scoops (60g) protein powder
+1 cup blueberries
+10-30g Sweetener (depends on how sweet your banana and protein is)
+1 tsp Cinnamon
+1 tsp Vanilla Extract
+1 tsp Baking powder- optional but recommended.', 1 );
+INSERT INTO recipes (img_path, title, instructions, description, ingredients, user_id)
+VALUES ('healthy_mcchicken.jpg','Healthy McChicken','Mix all of the ingredients for the patties 
+Coat patties with 10g of Corn flake crumbs 
+Air fry at 390 for 10 minutes','Per Sandwich: 196 Calories | 32g Protein| 4g Fat | 9g Net Carbs', 'Ingredients for 6 Sandwiches: 
+600g Chicken Breast ( I used Tenderloin for lower calorie)
+1 tbsp Mustard
+1tsp Paprika
+1tsp Cayenne
+2tsp Garlic Powder
+1 tsp Onion Powder
+1tsp Salt
+2 tsp Black Pepper
+1 tsp Celery powder
+Each patty picked up about 10g Corn flake crumbs
+
+Low calorie bun ( I used Culture Keto Buns found at Walmart)
+Romaine Lettuce
+7g of Light Mayo- I used Miracle Whip light mayo', 1 );
+INSERT INTO recipes (img_path, title, instructions, description, ingredients, user_id)
+VALUES ('cinnamon_bagel.jpg','Protein Cinnamon Crunch Bagel','Mix the greek yogurt, flour and cinnamon
+Add splenda brown sugar and cinnamon on top of each bagel
+Bake in the oven at 350 for 27-30 minutes','Each: 335 Calories l 18g Protein l 0g Fat l 61g Carbs', '150g Flour
+170g Greek Yogurt
+1 tsp Baking Powder
+Splenda brown sugar with cinnamon
+Egg Wash', 1 );
+INSERT INTO recipes (img_path, title, instructions, description, ingredients, user_id)
+VALUES ('korean_chicken.jpg','Air Fried Korean Fried Chicken','Cube up the chicken breast
+Soak the chicken breast in the brine for a few hours
+Coat the chicken in the batter
+Air Fry at 390 for 10 minutes
+Chose one of the sauces and put it in a pan on medium
+Then put the chicken in the pan','553 Calories | 86g Protein | 4g Fat | 34g Carbs', '400g chicken breast
+Batter:
+1 tsp ginger
+1 tsp garlic powder
+1 tsp onion powder
+1 tsp Baking Powder
+40g Potato Starch
+30g Cake Flour
+
+Brine:
+1 tsp garlic powder
+1 tsp onion powder
+2 tbsp white vinegar
+2.5 cups water
+
+Soy Garlic Sauce: (15 Calories)
+1 tbsp soy sauce
+1.5 tbsp sugar free maple syrup
+3 garlic gloves or 1tbsp minced garlic
+Black pepper
+Sesame seeds- option
+Green onions- optional
+
+Sweet Gochujang Sauce: (20 Calories)
+1 tbsp Gochujang
+4 tbsp water
+0 Calories Monkfruit Sweetener ( you may use any sweetener)', 1 );
